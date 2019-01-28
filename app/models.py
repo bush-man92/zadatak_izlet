d8898ce1ace74490c0cb307579367bf3e4d8aa5f
SergioRaskovic <45363238+SergioRaskovic@users.noreply.github.com>
2019-01-28 15:32:08 +0100

    baza

--- app/models.py
+++ app/models.py
@@ -32,8 +32,16 @@ class Trip(db.Model):
     max_people = db.Column(db.String(120),index=True)
     total_cost = db.Column(db.Integer,index=True)
     creator_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    trip_rating = db.Column(db.Integer, nullable=True)
 
-class User_rating(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_rating = db.Column(db.Integer)
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+class Comments(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    comments = db.Column(db.String(120), index= True)
+
+user_trip = db.Table('user_trip',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('trip_id', db.Integer, db.ForeignKey('trip.id'), primary_key=True),
+    db.Column('comments_id', db.Integer, db.ForeignKey('comments.id'), primary_key=True)
+)
+
+
